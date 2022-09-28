30edd795f4a5dc85b97dcff9fb50955b0078853e
JuaGon653 <106782112+JuaGon653@users.noreply.github.com>
2022-09-27 21:04:04 -0500

    created POST request to check login credentials

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -20,4 +20,32 @@ router.post('/', async (req,res) => {
     }
 });
 
+router.post('/login', async (req, res) => {
+    try {
+        const userData = await User.findOne({
+            where: {
+                username: req.body.username
+            }
+        });
+
+        if(!userData) {
+            res.status(400).json({ message: 'Incorrect email or password. Please try again!' });
+        };
+
+        const validPassword = await userData.checkPassword(req.body.password);
+
+        if(!validPassword) {
+            res.status(400).json({ message: 'Incorrect email or password. Please try again!' });
+        };
+
+        req.session.save(() => {
+            req.session.logged_in = true;
+
+            res.status(200).json({ user: userData, message: 'You are now logged in!' });
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+})
+
 module.exports = router;
