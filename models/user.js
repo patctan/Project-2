8aa8cd12b7ebd7e3d91458201f77b48730acf459
Jacob <jacobschwal@gmail.com>
2022-09-27 19:33:11 -0500

    adding password bcrypt and planner models

--- models/user.js
+++ models/user.js
@@ -2,6 +2,12 @@ const { Model, DataTypes } = require('sequelize');
 const bcrypt = require('bcrypt');
 const sequelize = require('../config/connection');
 
+class User extends Model {
+    checkPassword(loginPw) {
+        return bcrypt.compareSync(loginPw, this.password);
+    }
+}
+
 User.init(
     {
         id: {
@@ -30,4 +36,7 @@ User.init(
             },
         },
     },
-)
+);
+
+module.exports = user;
+
