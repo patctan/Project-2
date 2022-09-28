a6e6e3b3fa4db4556fa2c541158ba84d0852eb73
JuaGon653 <106782112+JuaGon653@users.noreply.github.com>
2022-09-27 20:59:00 -0500

    created POST request to create a user

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -2,5 +2,22 @@ const router = require('express').Router();
 const { User } = require('../../models');
 
 // requests go here
+router.post('/', async (req,res) => {
+    try {
+        const userData = await User.create({
+            username: req.body.username,
+            email: req.body.email,
+            password: req.body.password
+        });
+
+        req.session.save(() => {
+            req.session.logged_in = true;
+
+            res.status(200).json(userData);
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
 
 module.exports = router;
