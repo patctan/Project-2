bd7d368c81045f65827996661d6558675e45d6d1
JuaGon653 <106782112+JuaGon653@users.noreply.github.com>
2022-09-27 21:06:08 -0500

    created POST request to end a session when user logs out

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -46,6 +46,16 @@ router.post('/login', async (req, res) => {
     } catch (err) {
         res.status(500).json(err);
     }
-})
+});
+
+router.post('/logout', (req, res) => {
+    if (req.session.logged_in) {
+        req.session.destroy(() => {
+            res.status(204).end();
+        });
+    } else {
+        res.status(404).end();
+    }
+});
 
 module.exports = router;
