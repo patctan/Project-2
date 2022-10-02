0e4b8b3bd3133febeb08cb87dab5d64010699934
Bitaw <stickynotes5@yahoo.com>
2022-10-02 18:58:11 -0500

    Made changes to seed.js so Project can be viewed from Insomnia within branch

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,21 +1,23 @@
-const sequalize = require('../config/connection');
-const {User, Planner} = require('../models');
+const sequalize = require("../config/connection");
+const { User, Planner } = require("../models");
 
-const userData = require('./userData.json');
-const plannerData = require('./plannerData.json');
+const userData = require("./userData.json");
+const plannerData = require("./plannerData.json");
 
 const seedDatabase = async () => {
-    await sequalize.sync({force: true});
+  await sequalize.sync({ force: true });
 
-    const users = await User.bulkCreate(userData, {
-        individualHooks: true,
-        returning: true,
+  const users = await User.bulkCreate(userData, {
+    individualHooks: true,
+    returning: true,
+  });
+
+  for (const planner of plannerData) {
+    await Planner.create({
+      ...planner,
+      user_id: users[Math.floor(Math.random() * users.length)].id,
     });
-    
-    for (const planner of plannerData) {
-        await Planner.create({
-            ...planner,
-            user_id: users[Math.floor(Math.random() * users.length)].id,
-        });
-    }
-}
+  }
+};
+
+seedDatabase();
