94144121fd0fc1d479f722408ff87f3f5c54da7d
Bitaw <stickynotes5@yahoo.com>
2022-10-07 17:13:21 -0500

    changed './User' in index.js to lowercase

--- models/index.js
+++ models/index.js
@@ -1,13 +1,13 @@
-const User = require('./User');
-const Planner = require('./Planner');
+const User = require("./user");
+const Planner = require("./Planner");
 
 User.hasMany(Planner, {
-    foreignKey:'user_id',
-    onDelete: 'CASCADE'
+  foreignKey: "user_id",
+  onDelete: "CASCADE",
 });
 
 Planner.belongsTo(User, {
-    foreignKey: 'user_id'
+  foreignKey: "user_id",
 });
 
-module.exports = {User, Planner};
+module.exports = { User, Planner };
